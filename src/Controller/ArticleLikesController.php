937b6b0b3c2d43b606c17e606eedf6835d12392d
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-10-13 00:51:22 +0300

    lesson 1.9: add article likes change

---
+++ src/Controller/ArticleLikesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleLikesController extends AbstractController
+{
+    /**
+     * @Route("/articles/{articleId<\d+>}/likes", name="app_article_like_change", methods={"PUT","DELETE"})
+     */
+    public function change(string $articleId, Request $request)
+    {
+        $likes = match ($request->getMethod()) {
+            Request::METHOD_PUT => 1,
+            Request::METHOD_DELETE => 0,
+
+            default => throw new BadRequestHttpException('Bad request'),
+        };
+
+        return $this->json(['likes' => $likes]);
+    }
+}
