527b8e500ffee7a788c2d60c29d70faae48c05c4
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-10-19 10:13:29 +0300

    lesson 2.4: setup dependencies

--- src/Service/MarkdownParser.php
+++ src/Service/MarkdownParser.php
@@ -13,18 +13,24 @@ class MarkdownParser
     protected Parsedown $parsedown;
     protected AdapterInterface $cache;
     protected LoggerInterface $logger;
+    protected bool $debug;
 
-    public function __construct(Parsedown $parsedown, AdapterInterface $cache, LoggerInterface $logger)
+    public function __construct(Parsedown $parsedown, AdapterInterface $cache, LoggerInterface $logger, bool $debug)
     {
         $this->parsedown = $parsedown;
         $this->cache = $cache;
         $this->logger = $logger;
+        $this->debug = $debug;
     }
 
     public function parse(string $content): string
     {
-        if (stripos($content, 'lorem') !== false) {
-            $this->logger->info('Lorem found!');
+        if ($this->debug) {
+            if (stripos($content, 'lorem') !== false) {
+                $this->logger->info('Lorem found!');
+            }
+
+            return $this->parsedown->parse($content);
         }
 
         return $this->cache->get(
