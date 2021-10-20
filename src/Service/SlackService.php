7ed4a6f45b4c6d2e27ef211971ae501e361e3559
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-10-20 08:13:01 +0300

    lesson 2.6: add @required setter injection

--- src/Service/SlackService.php
+++ src/Service/SlackService.php
@@ -5,18 +5,30 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Nexy\Slack\Client;
+use Psr\Log\LoggerInterface;
 
 class SlackService
 {
     protected Client $client;
+    protected LoggerInterface $logger;
 
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * @required
+     */
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+    }
+
     public function sendMessage(string $text, string $author = 'Bot', string $icon = ':ghost:'): void
     {
+        $this->logger?->info($text);
+
         //$client = $this->get('nexy_slack.client');
         $message = $this->client->createMessage();
 
