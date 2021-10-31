fd8e36b09df14ccd8afeb08b14ba5a455fede71c
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-11-01 01:38:08 +0200

    lesson 7.2: add paginator

--- src/Controller/Admin/CommentsController.php
+++ src/Controller/Admin/CommentsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Repository\CommentRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,15 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentsController extends AbstractController
 {
     #[Route('/admin/comments', name: 'app_admin_comments')]
-    public function index(Request $request, CommentRepository $commentRepository): Response
+    public function index(Request $request, CommentRepository $commentRepository, PaginatorInterface $paginator): Response
     {
-        $comments = $commentRepository->findAllWithSearch(
-            $request->query->get('q'),
-            $request->query->has('showDeleted'),
+        $pagination = $paginator->paginate(
+            $commentRepository->findAllWithSearchQuery(
+                $request->query->get('q'),
+                $request->query->has('showDeleted'),
+            ),
+            $request->query->getInt('page', 1),
+            10
         );
 
         return $this->render('admin/comments/comments.html.twig', [
-            'comments' => $comments,
+            'pagination' => $pagination,
         ]);
     }
 }
