216488f8fe5d9d6b9af8a45a0546bada96d21dd3
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-11-02 19:10:02 +0200

    lesson 8.2: add authenticator, login form, css

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    protected function getLoginUrl(Request $request): string
+    {
+        // TODO: Implement getLoginUrl() method.
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        // TODO: Implement authenticate() method.
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+}
