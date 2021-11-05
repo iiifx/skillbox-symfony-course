73c628a41eb2f3d86df4f1f08327cdab1d25a90e
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-11-05 19:24:18 +0200

    lesson 10.1: add serializer, api token entity

---
+++ src/Controller/Api/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
+class UserController extends AbstractController
+{
+    #[Route('/api/user', name: 'api_user')]
+    public function index(): Response
+    {
+        return $this->json([
+            'user' => $this->getUser(),
+        ], 200, [], ['groups' => 'main']);
+    }
+}
