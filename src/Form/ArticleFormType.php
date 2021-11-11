9a0040df7c49e3c3264865e39c51f21863b9df74
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-11-11 21:16:27 +0200

    lesson 12.2: add form field type, transformers

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,21 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body');
+            ->add('title', TextType::class, [
+                'label' => 'Название статьи',
+                'help' => 'Не используйте слово "собака"',
+            ])
+            ->add('body')
+            ->add('publishedAt', options: [
+                'widget' => 'single_text',
+            ]);
+
+        $builder->get('body')->addModelTransformer(
+            new CallbackTransformer(
+                static fn($bodyFromDb) => str_replace('**собака**', 'собака', $bodyFromDb),
+                static fn($bodyFromInput) => str_replace('собака', '**собака**', $bodyFromInput),
+            )
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
