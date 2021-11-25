30e7d7e0294e6be0c9f05848a9682c2071f87c38
Khomenko Vitaliy <iiifx.me@gmail.com>
2021-11-25 19:35:44 +0200

    lesson 18.2: bundle setup

--- lib/ArticleContentProviderBundle/src/ArticleContentProviderBundle.php
+++ lib/ArticleContentProviderBundle/src/ArticleContentProviderBundle.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace SkillboxSymfony\ArticleContentProviderBundle;
 
+use SkillboxSymfony\ArticleContentProviderBundle\DependencyInjection\ArticleContentProviderExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ArticleContentProviderBundle extends Bundle
 {
-
+    public function getContainerExtension()
+    {
+        return $this->extension ??= new ArticleContentProviderExtension();
+    }
 }
--- lib/ArticleContentProviderBundle/src/DependencyInjection/ArticleContentProviderExtension.php
+++ lib/ArticleContentProviderBundle/src/DependencyInjection/ArticleContentProviderExtension.php
@@ -11,9 +11,23 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class ArticleContentProviderExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public const ALIAS = 'skillbox_article_content_provider';
+
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
         $loader->load('services.xml');
+
+        if ($configuration = $this->getConfiguration($configs, $container)) {
+            $config = $this->processConfiguration($configuration, $configs);
+
+            $definition = $container->getDefinition('skillbox.article_content_provider');
+            $definition->setArgument(1, $config['is_bold']);
+        }
+    }
+
+    public function getAlias(): string
+    {
+        return self::ALIAS;
     }
 }
